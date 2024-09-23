ae4ca28c2221f6e233e294e09e4cd7cb1edda3d9
nguyentran <nguyentt@hopee.com.vn>
2024-09-23 09:12:21 +0700

    HIKARIACADEMYELEARNING-48 Push code home page and other layouts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,21 +5,63 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Hikari elearning</title>
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    @yield('styles')
+
+    <style>
+        header#header {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            z-index: 100000000000;
+            background: #ffffff;
+        }
+
+        aside#sidebar {
+            position: sticky;
+            top: 60px;
+            left: 0;
+            width: 120px;
+            height: 100%;
+            z-index: 1000;
+            background: #ffffff;
+        }
+
+        .main-content {
+            margin-top: 60px;
+            position: relative;
+        }
+    </style>
 </head>
-<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-@yield('styles')
+
 <body>
-    <header id="header">
-        @include('layouts.header')
-    </header>
-    <div id="app">
-        @yield('content')
+    <div class="layout-wrapper">
+        <header id="header">
+            @include('layouts.header')
+        </header>
+        <div class="d-flex">
+            <aside class="sidebar" id="sidebar" style="height: 100%">
+                @include('layouts.sidebar')
+            </aside>
+            <div class="container-fluid main-content">
+                <div id="main-wrapper">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
     </div>
+
     <footer id="footer">
         @include('layouts.footer')
     </footer>
+
     <script src="{{ asset('js/app.js') }}"></script>
     @yield('scripts')
+    <script>
+
+    </script>
 </body>
+
 </html>
