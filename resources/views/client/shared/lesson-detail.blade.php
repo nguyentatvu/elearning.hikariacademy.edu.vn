545f73d7f5b3abdfca2f01320d61f6ac10f9d477
nguyentran <nguyentt@hopee.com.vn>
2024-10-29 14:45:45 +0700

    fix show description

--- resources/views/client/shared/lesson-detail.blade.php
+++ resources/views/client/shared/lesson-detail.blade.php
@@ -218,10 +218,10 @@
         const showBuyCourseModal = () => $('#buy_course_modal').modal('show');
 
         $(document).ready(function() {
-            let isHidden = false;
+            let isHidden = true;
 
             @if (isset($flashcardDetail) && !empty($flashcardDetail))
-                isHidden = true;
+                isHidden = false;
                 $('#btn_hide_tab_content').find('i').toggleClass('bi-chevron-double-up bi-chevron-double-down');
             @endif
 
