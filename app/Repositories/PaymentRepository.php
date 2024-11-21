064161f89c0d938057256e6f7b2e3876b209040c
hungnt-hopee <hungnt@hopee.com.vn>
2024-11-21 13:20:10 +0700

    Fix bug list current learning courses in details learning of admin ui

--- app/Repositories/PaymentRepository.php
+++ app/Repositories/PaymentRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Payment;
+use App\PaymentMethod;
 
 class PaymentRepository extends BaseRepository
 {
@@ -19,13 +20,12 @@ class PaymentRepository extends BaseRepository
     {
         $payments = $this->model::where('user_id', $studentId)
             ->with('paymentMethod', 'series')
+            ->whereHas('paymentMethod', function ($query) {
+                $query->where('status', PaymentMethod::PAYMENT_SUCCESS);
+            })
             ->select('id', 'user_id', 'item_id', 'payments_method_id', 'time', 'created_at')
             ->orderBy('created_at', 'desc')
             ->get()
-            ->groupBy('item_id')
-            ->map(function ($group) {
-                return $group->first();
-            })
             ->values();
 
         return $payments;
